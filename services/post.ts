e4245c1dea2483b3eb9859689fea2e73d158afc5
PhamPhien-gif <toilaphien2302@gmail.com>
2025-12-13 22:35:19 +0700

    chỉnh lại model

--- services/post.ts
+++ services/post.ts
@@ -1,48 +1,6 @@
+import { type CommentsResponse, type PostItem, type PostsResponse } from '@/models/post';
 import { api } from './api';
-import { User } from './user';
-// --- Interfaces (Định nghĩa kiểu dữ liệu trả về) ---
 
-export interface Media {
-  id: number;
-  media_url: string;
-}
-
-
-export interface PostItem {
-  id: number;
-  content: string;
-  user: User;
-  media: Media[];
-  reactions_count: number;
-  comments_count: number;
-  shares_count: number;
-  created_at: Date;
-  is_liked: boolean;
-  is_shared: boolean;
-  // ... các trường khác của post
-}
-
-export interface PostsResponse {
-  data: PostItem[];
-  nextCursor?: string;
-  hasMore: boolean;
-}
-
-export interface Comment {
-  id: number;
-  post_id: number;
-  content: string;
-  parent_comment_id: number | null;
-  created_at: string;
-  user: User;
-  is_liked: boolean;
-  reactions_count: number;
-  children_recursive: Comment[];
-}
-
-export interface CommentsResponse {
-  data: Comment[];
-}
 
 // --- Service Logic ---
 
